ec2d1706338b71c7943200159bd316c76681cc5a
Dandi29022000 <sembarangboleh55@gmail.com>
2021-04-12 17:23:20 +0700

    Upload file pada pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\CariMahasiswaController;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', [HomeController::class, 'index']);
+
+// Route::get('/about', [AboutController::class, 'about']);
+
+// Route::get('/article/{id}', [ArticleController::class, 'article']);
+
+Route::resource('mahasiswas', MahasiswaController::class);
