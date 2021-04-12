232550244ab1f63dd1fe56631a1d29c4d7d1954e
Dandi29022000 <sembarangboleh55@gmail.com>
2021-04-12 19:44:12 +0700

    Mengubah tampilan view

--- resources/views/mahasiswas/create.blade.php
+++ resources/views/mahasiswas/create.blade.php
@@ -33,8 +33,13 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="Kelas">Kelas</label> 
-                            <input type="Kelas" name="Kelas" class="form-control" id="Kelas" aria-describedby="Kelas" >
+                            <label for="Kelas">Kelas</label>
+                            <select name="Kelas" id="Kelas" class="form-control">
+                                <option selected disabled>Pilih Kelas</option>
+                            @foreach($kelas as $Kelas)
+                                <option value="{{ $Kelas->id }}">{{ $Kelas->nama_kelas }}</option>
+                            @endforeach
+                            </select>
                         </div>
 
                         <div class="form-group">
